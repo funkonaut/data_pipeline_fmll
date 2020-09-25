722a2254e27e266151abe98e786abbb9f837fcfa
funkonaut <crcorrell@gmail.com>
2020-09-25 08:41:46 -0500

    Add files via upload

--- data_preprocess.py
+++ data_preprocess.py
@@ -23,9 +23,24 @@ df_tcad['zip']       = convert_to_numeric(df_tcad,'zip')
 df_tcad['mail_zip']  = convert_to_numeric(df_tcad,'mail_zip')
 df_tcad['geo_id']    = convert_to_numeric(df_tcad,'geo_id')
 gdf_16['prop_id']    = gdf_16['prop_id'].astype(int) 
-gdf['property_i']       = convert_to_numeric(gdf,'property_i')
+gdf['property_i']    = convert_to_numeric(gdf,'property_i')
 
 #****MAYBE CAN GET RID OF MORE**** Get rid of rows with no useful info
 gdf_16 = gdf_16.loc[gdf_16.prop_id!=0]
 gdf    = gdf.loc[gdf.property_i!=0]
 gdf.rename(columns={ gdf.columns[1]: "prop_id" }, inplace = True)
+
+#Todo sum up unit count and use this data to fill in missing values for locations and units Cleanup construction permit data only residential
+df_con = df_con.loc[(df_con['Permit Class Mapped']=='Residential')]
+#Get rid of rows with no useful info
+df_con = df_con.loc[df_con['TCAD ID'].notnull()&df_con['Latitude'].notnull()&df_con['Housing Units'].notnull()]
+df_con = df_con.loc[df_con['Work Class']!='Demolition']
+df_con['TCAD ID'] = extract_num(df_con,'TCAD ID')
+keep = ['TCAD ID','Latitude','Longitude']
+df_con = df_con[keep]
+df_con.drop_duplicates('TCAD ID',inplace = True)
+
+#Eviction Data
+#Get rid of rows with no useful info around 500 just case numbers or nothing at all
+df_evict.drop(df_evict.loc[df_evict.Lat.isnull() & df_evict['Property ID'].isnull()].index,inplace=True)
+clean_up(df_evict,['Unnamed: 12' , 'Unnamed: 13' , 'Unnamed: 14'],delete=True)
