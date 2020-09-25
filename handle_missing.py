99450e6e676a5f241c02b31abfa3bc95ce540997
funkonaut <crcorrell@gmail.com>
2020-09-25 08:42:42 -0500

    Add files via upload

--- handle_missing.py
+++ handle_missing.py
@@ -1,6 +1,8 @@
 ########################
 ###  HANDLE MISSING  ###
 ########################
+#Fill out known values for location
+df_tcad['known_loc'] = np.where(df_tcad.X_x.notnull(),True,False)
 #Fill in missing geometry is this working correctly? Various locations and somethings getting filled that are not really the same?
 df_tcad.geometry_y = df_tcad.groupby('address').geometry_y.transform('ffill')
 df_tcad.geometry_y = df_tcad.groupby('address').geometry_y.transform('bfill')
@@ -43,44 +45,24 @@ df_tcad.units_x = np.where((df_tcad.value>10000000)&(df_tcad.units_x<5),np.nan,d
 #Write known column where units are correct or rather better estimates 
 df_tcad['known'] = np.where(df_tcad.units_x.notnull(),True,False)
 
-#reset unit count back to known
-#df_tcad['units_x'] = np.where(df_tcad.known,df_tcad.units_x,np.nan)
-#del df_tcad['avg_unit_cost_zip']
-#del df_tcad['avg_unit_cost']
+#Only Residential form now on
+res_type = ['DORMITORY HIRISE', 'DORMITORY','APARTMENT   5-25','LUXURY HI-RISE APTS 100+', 'APARTMENT 100+'] 
+#df_tcad['res'] = (df_tcad.res | df_tcad.type1.isin(res_type)) 
+df_tcad['res'] = (df_tcad.res | df_tcad.type1.isin(res_type) | (df_tcad.type1.str.contains('CONDO',na=False))) 
+df_res = df_tcad.loc[df_tcad.res] 
 
-#calculate missing units for res
-res_type = ['DORMITORY HIRISE', 'DORMITORY','APARTMENT   5-25','LUXURY HI-RISE APTS 100+', 'APARTMENT 100+']
-df_tcad['res'] = (df_tcad.res | df_tcad.type1.isin(res_type))
-df_res = df_tcad.loc[df_tcad.res]
+#Impute by zip and appraisal value 
+dfs = df_res.groupby('zip').apply(lambda df: mice(df[['units_x','value','prop_id']].values) if df[['units_x','value','prop_id']].units_x.isnull().any() & ~df[['units_x','value','prop_id']].units_x.isnull().all() else df).apply(lambda df: pd.DataFrame(df))
+df_impute_zip = pd.concat(dfs.values)        
+#Merge it back and fill it in (smart: if not lower then low estimate from imp data)
+df_impute_zip = df_impute_zip.loc[df_impute_zip[0].notnull()][[0,1,2]]
+df_impute_zip.columns = ['unit_est','value','prop_id']
+df_res = df_res.merge(df_impute_zip,on=['prop_id','value'],how='left')
+df_res.units_x.fillna(df_res.unit_est,inplace=True)
+df_res.units_x = np.where((df_res.low>df_res.units_x)&(~df_res.known),df_res.low,df_res.units_x)
+#Round it and correct 
+df_res.units_x = round(df_res.units_x)
+df_res.units_x = np.where(df_res.units_x<1, 1, df_res.units_x)
 
-#this takes around 5 min
-train = df_res[['units_x','value','zip']]
-df_impute = mice(train.values)
-df_impute = pd.DataFrame(df_impute, columns = ['units_x','value','zip'])
-df_res.reset_index(inplace=True)
-df_res.units_x.fillna(df_impute.units_x,inplace=True)
-
-#ESTIMATE THE REST
-#Anything under 10m is assumed 1 unit
-df_tcad.units_x = np.where((df_tcad.value<10000000)&(df_tcad.units_x.isnull()),1,df_tcad.units_x)
-df_tcad['est'] = np.where(df_tcad.units_x.isnull(),True,False)
-
-#calculate avg unit cost for known residential units 
-##will I get a more accurate answer if I look at 2016 unit cost? or just assume 2016 numbers are mostly consitent?
-df_known = df_tcad.loc[df_tcad.known]
-##get each props unit cost
-df_known['unit_cost'] = df_known.value/df_known.units_x
-##group by to calculate average cost per prop type in a sub_div
-df_known['avg_unit_cost'] = df_known.groupby(['sub_div'])['unit_cost'].transform('mean')
-df_known['avg_unit_cost_zip'] = df_known.groupby(['zip'])['unit_cost'].transform('mean')
-clean_up(df_known,['prop_id','avg_unit_cost','avg_unit_cost_zip'])
-df_tcad = df_tcad.merge(df_known,on='prop_id',how='left').drop_duplicates('prop_id')
-df_tcad.avg_unit_cost = df_tcad.groupby(['sub_div'])['avg_unit_cost'].transform('ffill')
-df_tcad.avg_unit_cost = df_tcad.groupby(['sub_div'])['avg_unit_cost'].transform('bfill')
-df_tcad.avg_unit_cost_zip = df_tcad.groupby(['zip'])['avg_unit_cost_zip'].transform('ffill')
-df_tcad.avg_unit_cost_zip = df_tcad.groupby(['zip'])['avg_unit_cost_zip'].transform('bfill')
-df_tcad.units_x.fillna(round(df_tcad.value/df_tcad.avg_unit_cost),inplace=True)
-df_tcad.units_x.replace(0,1,inplace=True) #round anything that is 0 up to 1
-##fill in what wasnt caught by subdiv and land_use groupings with zip groupings
-df_tcad.units_x.fillna(round(df_tcad.value/df_tcad.avg_unit_cost_zip),inplace=True)
-df_tcad.units_x.replace(0,1,inplace=True) #round anything that is 0 up to 1
+#***^THIS CAN BE IMPROVED LOOK INTO https://scikit-learn.org/stable/auto_examples/impute/plot_missing_values.html#sphx-glr-auto-examples-impute-plot-missing-values-py***
+# https://analyticsweek.com/content/iterative-imputation-for-missing-values-in-machine-learning/
